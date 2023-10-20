126a15d2a7dc406333e17d1fe00580639997f0cd
Agzjy <135308654+Agzjy@users.noreply.github.com>
2023-10-19 22:26:56 -0400

    Update index.js

--- src/Kanbas/Database/index.js
+++ src/Kanbas/Database/index.js
@@ -5,7 +5,7 @@ import users from "./users.json";
 import enrollments from "./enrollments.json";
 import grades from "./grades.json";
 
-export default {
+const database = {
   courses,
   modules,
   assignments,
