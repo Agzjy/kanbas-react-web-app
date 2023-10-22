70bf42c1efeea01a47be310d31f5786dfb7a7c2d
Agzjy <135308654+Agzjy@users.noreply.github.com>
2023-10-21 23:46:17 -0400

    Update index.js

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -1,19 +1,38 @@
-import { Link } from "react-router-dom";
 import db from "../Database";
-import './index.css';
+import { Link } from "react-router-dom";
+
 function Dashboard() {
   const courses = db.courses;
+
   return (
     <div>
       <h1>Dashboard</h1>
-      <div className="list-group">
-        {courses.map((course) => (
-          <Link key={course._id} to={`/Kanbas/Courses/${course._id}`} className="list-group-item">
-            {course.name}
-          </Link>
+      <hr />
+      <h2>Published Courses ({courses.length})</h2>
+      <div className="row">
+        {courses.map((course, index) => (
+          <div className="col-lg-3 col-md-4 col-sm-6 col-6 mb-4" key={index}>
+            <div className="card">
+              <div className="card-header bg-primary text-white">
+                {/* <h5 className="card-title">{course.name}</h5> */} 
+              </div>
+              <div className="card-body">
+                <Link
+                  to={`/Kanbas/Courses/${course._id}`}
+                >
+                  {course.name}
+                </Link>
+                <p className="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content.
+                </p>
+              </div>
+            </div>
+          </div>
         ))}
       </div>
     </div>
   );
 }
+
 export default Dashboard;
